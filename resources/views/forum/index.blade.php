6f21ec9bc353a9b66b0506d5a646309716631bfe
Suhaayb <1197145@student.roc-nijmegen.nl>
2023-12-13 15:56:15 +0100

    Forum page created

---
+++ resources/views/forum/index.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/forum/index.blade.php -->
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Forum') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <!-- Your forum content goes here -->
+    </div>
+</x-app-layout>
