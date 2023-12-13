df01d50d7128466f573e05ec138ddc75001be2a7
Wessel <appelhofwessel@gmail.com>
2023-12-13 16:22:35 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,11 +38,26 @@
             @endif
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                            <div>
+                            <div class="text-center">
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Bullet Maze</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    Informatie over de game
+                                    Welkom bij "Levelling Maze" - het ultieme avontuurlijke spel met drie uitdagende levels die wachten om overwonnen te worden!
+                                    <br>
+                                    <br>
+                                    🌟 Ontdek de Levels: Navigeer door intrigerende doolhoven vol verrassende wendingen.
+                                    <br>
+                                    <br>
+                                    🎮 Unieke Gameplay: Combineert actie en strategie voor een meeslepende ervaring die je behendigheid en denkvermogen test.
+                                    <br>
+                                    <br>
+                                    💎 Verzamel Beloningen: Ontgrendel krachtige beloningen en power-ups bij elke overwinning.
+                                    <br>
+                                    <br>
+                                    🏆 Word de Kampioen: Alleen de moedigsten zullen de titel van kampioen verdienen. Durf jij de uitdaging aan?
+                                    <br>
+                                    <br>
+                                    🚀 Speel Nu: Betreed de Levelling Maze, toon je vaardigheden, overwin de doolhoven en claim je plaats als de ultieme kampioen!
                                 </p>
                             </div>
                         </div>
