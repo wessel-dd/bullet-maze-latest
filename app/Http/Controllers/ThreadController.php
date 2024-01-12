7eb6bf7a588e7e3f8c60da718a254dfcfce92960
Suhaayb <1197145@student.roc-nijmegen.nl>
2024-01-12 14:54:09 +0100

    Version 1 forum creation
    Kan later aangepast worden maar dit is voor nu de manier waarop er forum posts gemaakt worden

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -2,9 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Thread;
+use Auth;
 
 class ThreadController extends Controller
 {
-    //
+    public function index()
+    {
+        $threads = Thread::all();
+        return view('forum.index', ['threads' => $threads]);
+    }
+    public function create()
+    {
+        return view('forum.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'text' => 'required|string'
+        ]);
+
+
+        $thread = new Thread();
+        $thread->title = $request->title;
+        $thread->text = $request->text;
+        $thread->user_id = Auth::id();
+
+        $thread->save();
+
+
+        return redirect()->route('threads.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
 }
